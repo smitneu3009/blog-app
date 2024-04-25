a39bbd690abda3eeda08a737b63ef765fe80dc7b
Smit Patel <patel.smit5@northeastern.edu>
2024-04-25 10:50:12 -0400

    initial setup

---
+++ myproject/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, Text, StatusBar, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
