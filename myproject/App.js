b22019358c30ec82cb62ab7e47492914d35c8bc0
Smit Patel <patel.smit5@northeastern.edu>
2024-05-03 09:28:58 -0400

    signin screens

--- myproject/App.js
+++ myproject/App.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import { View, Text, StatusBar, StyleSheet } from 'react-native';
+import React,{useState} from 'react';
+import { SafeAreaView,StyleSheet,Text } from 'react-native';
+
+import Navigation from './src/navigation';
 
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.root}>
+      <Navigation/>
+    </SafeAreaView>
   );
-}
+};
+
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  root:{
+    flex:1,
+    backgroundColor:'#F9FBFC'
+  }
+})
+
+
---
+++ myproject/src/screens/SignUpScreen/SignUpScreen.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import CustomInput from "../../components/CustomInput";
+import CustomButton from "../../components/CustomButton/CustomButton";
+import SocialSignInButtons from "../../components/SocialSignInButtons/SocialSignInButtons";
+import { useNavigation } from "@react-navigation/native";
+
+const SignUpScreen = () => {
+  const { username, setUsername } = useState("");
+  const { email, setEmail } = useState("");
+  const { password, setPassword } = useState("");
+  const { passwordRepeat, setPasswordRepeat } = useState("");
+
+  const navigation = useNavigation();
+
+  const onRegisterPressed = () => {
+    navigation.navigate('ConfirmEmail')
+  };
+  const onSignInPressed = () => {
+    navigation.navigate('SignIn')
+  };
+  const onTermsOfUsePressed = () => {
+    console.warn("");
+  };
+  const onPrivacyPressed = () => {
+    console.warn("");
+  };
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={styles.root}>
+        <Text style={styles.title}>Create an account</Text>
+        <CustomInput
+          placeholder="Username"
+          value={username}
+          setValue={setUsername}
+        />
+        <CustomInput placeholder="Email" value={email} setValue={setEmail} />
+        <CustomInput
+          placeholder="Password"
+          value={password}
+          setValue={setPassword}
+          secureTextEntry={true}
+        />
+        <CustomInput
+          placeholder="Repeat Password"
+          value={passwordRepeat}
+          setValue={setPasswordRepeat}
+          secureTextEntry={true}
+        />
+        <CustomButton text="Register" onPress={onRegisterPressed} />
+        <Text style={styles.text}>
+          By registereing, you confirm that you accept our{' '} <Text style={styles.link} onPress={onTermsOfUsePressed}>Terms of Use</Text> and{' '}
+          <Text style={styles.link} onPress={onPrivacyPressed}>Privacy Policy</Text>
+        </Text>
+        
+        <SocialSignInButtons/>
+        <CustomButton
+          text="Have an account? Sign in"
+          onPress={onSignInPressed}
+          type="TERTIARY"
+        />
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    alignItems: "center",
+    padding: 20,
+    marginTop: 35,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "#051C60",
+    margin: 10,
+  },
+  text:{
+    color:'gray',
+    marginVertical:11,
+  },
+  link:{
+    color: '#FDB075'
+  }
+});
+
+export default SignUpScreen;
